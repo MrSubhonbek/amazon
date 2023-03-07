d1818f175c630fef33dd66e9e527e78e6e5d829d
subhonbek <basubhonbek@gmail.com>
2023-03-07 10:48:36 +0300

    db favorite

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,7 +1,46 @@
-import { Controller } from '@nestjs/common';
-import { UserService } from './user.service';
+import {
+	Controller,
+	Param,
+	UsePipes,
+	Body,
+	HttpCode,
+	ValidationPipe,
+	Get,
+	Put,
+	Patch,
+} from '@nestjs/common'
+
+import { Auth } from 'src/auth/decorators/auth.decorator'
+import { CurrentUser } from 'src/auth/decorators/user.decorator'
+
+import { UserDto } from './user.dto'
+import { UserService } from './user.service'
 
 @Controller('user')
 export class UserController {
-  constructor(private readonly userService: UserService) {}
+	constructor(private readonly usersService: UserService) {}
+
+	@Get('profile')
+	@Auth()
+	async getProfile(@CurrentUser('id') id: number) {
+		return this.usersService.byId(id)
+	}
+
+	@UsePipes(new ValidationPipe())
+	@Auth()
+	@HttpCode(200)
+	@Put('profile')
+	async getNewToken(@CurrentUser('id') id: number, @Body() dto: UserDto) {
+		return this.usersService.updateProfile(id, dto)
+	}
+
+	@Auth()
+	@HttpCode(200)
+	@Patch('profile/favorite/:productId')
+	async toggleFavorite(
+		@Param('productId') productId: string,
+		@CurrentUser('id') id: number,
+	) {
+		return this.usersService.toggleFavorite(id, productId)
+	}
 }
