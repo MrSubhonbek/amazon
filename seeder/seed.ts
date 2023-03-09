1e4bd6bf5b888f45a9a5c2cb12413ac8c454dcb3
subhonbek <basubhonbek@gmail.com>
2023-03-09 09:47:00 +0300

    favirite and seed

--- seeder/seed.ts
+++ seeder/seed.ts
@@ -1,8 +1,6 @@
 import { faker } from '@faker-js/faker'
 import { PrismaClient, Product } from '@prisma/client'
 import * as dotenv from 'dotenv'
-import { slugify } from 'src/utils/generate-slug'
-import { getRandomNumber } from 'src/utils/random-number'
 
 dotenv.config()
 const prisma = new PrismaClient()
@@ -16,14 +14,43 @@ const createProducts = async (quantity: number) => {
 		const product = await prisma.product.create({
 			data: {
 				name: productName,
-				slug: slugify(productName),
-				price: +faker.commerce.price(10, 999, 0),
-				images: Array.from({ length: getRandomNumber(2, 6) }).map(() =>
-					faker.image.imageUrl(),
-				),
+				slug: faker.helpers.slugify(productName).toLocaleLowerCase(),
 				descriptios: faker.commerce.productDescription(),
+				price: +faker.commerce.price(10, 999, 0),
+				images: Array.from({
+					length: faker.datatype.number({ min: 2, max: 6 }),
+				}).map(() => faker.image.imageUrl()),
+				category: {
+					create: {
+						name: categoryName,
+						slug: faker.helpers.slugify(productName).toLocaleLowerCase(),
+					},
+				},
+				reviews: {
+					create: [
+						{
+							rating: faker.datatype.number({ min: 1, max: 5 }),
+							text: faker.lorem.paragraph(),
+							user: {
+								connect: {
+									id: 6,
+								},
+							},
+						},
+						{
+							rating: faker.datatype.number({ min: 1, max: 5 }),
+							text: faker.lorem.paragraph(),
+							user: {
+								connect: {
+									id: 6,
+								},
+							},
+						},
+					],
+				},
 			},
 		})
+		products.push(product)
 	}
 
 	console.log(`Created ${products.length} products`)
